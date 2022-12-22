01f0e3d40cbd3bb2fd41fb4e08936be998e55bb8
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2022-12-22 14:42:29 +0700

    update dashboard supplier

--- models/pemenang_model.js
+++ models/pemenang_model.js
@@ -63,7 +63,7 @@ class PemenangModel {
 
   static pemenangTenderByStatusTTDKontrak() {
     return new Promise((resolve) => {
-      const sql = `SELECT pemenang.* FROM tenderpl_tenderp.pemenang JOIN (SELECT jadwal.*, tahapan.nama_tahapan FROM tenderpl_tenderp.jadwal JOIN tenderpl_tenderp.tahapan ON jadwal.id_tahapan = tahapan.id_tahapan WHERE jadwal.id_tahapan = 12 AND DATEDIFF(CURDATE(), jadwal.tgl_mulai) <= 7) AS jadwal_tahapan ON pemenang.id_tender = jadwal_tahapan.id_tender;`;
+      const sql = `SELECT pemenang.*, tender.nama_tender, tender.nilai_kontrak, tender.nilai_hps FROM tenderpl_tenderp.pemenang JOIN (SELECT jadwal.*, tahapan.nama_tahapan FROM tenderpl_tenderp.jadwal JOIN tenderpl_tenderp.tahapan ON jadwal.id_tahapan = tahapan.id_tahapan WHERE jadwal.id_tahapan = 12 AND DATEDIFF(CURDATE(), jadwal.tgl_mulai) <= 7) AS jadwal_tahapan ON pemenang.id_tender = jadwal_tahapan.id_tender JOIN tenderpl_tenderp.tender ON pemenang.id_tender = tender.id_tender;`;
       database.query(sql, (err, result) => {
         resolve(result);
       });
